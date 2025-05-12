3d14f888623150f689caf58dfe06a1ab5a5f4c13
Jeffrey Ramo <matheusunitt@gmail.com>
2025-05-12 12:45:07 -0300

    Adiciona ActivityItem; Altera global.css;

--- src/components/Activity/index.jsx
+++ src/components/Activity/index.jsx
@@ -1,7 +1,16 @@
 import Section from "../Section";
+import ActivityItem from "../ActivityItem";
 
 const Activity = () => {
-  return <Section title="Atividade">oi</Section>;
+  return (
+    <Section title="Atividade">
+      <div className="flex flex-col gap-3">
+        <ActivityItem />
+        <ActivityItem />
+        <ActivityItem />
+      </div>
+    </Section>
+  );
 };
 
 export default Activity;
---
+++ src/components/ActivityItem/index.jsx
@@ -0,0 +1,15 @@
+const ActivityItem = () => {
+  return (
+    <div className="flex w-[450px] h-[68px] items-center justify-between px-10 py-3 text-[var(--fg-dark)] font-semibold bg-[var(--activity-item-color)] rounded-md shadow-md">
+      <div className="flex gap-3 items-center">
+        <i className="inline-block w-[15px] h-[15px] bg-gray-300 rounded-full"></i>
+        <span className="uppercase">Tarefa</span>
+      </div>
+      <div className="text-[var(--fg-subtle)] border-l-1 border-[var(--border-light)] pl-4">
+        0h
+      </div>
+    </div>
+  );
+};
+
+export default ActivityItem;
