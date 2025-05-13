63dba6eb286245d4b5aba0e59f2279ac455a5268
Jeffrey Ramo <matheusunitt@gmail.com>
2025-05-13 15:56:53 -0300

    Adiciona StatisticsMenu; Reorganiza Statistics;

--- src/components/Statistics/index.jsx
+++ src/components/Statistics/index.jsx
@@ -1,32 +1,37 @@
 import Section from "../Section";
 import StatisticsChart from "../StatisticsChart";
 import StatisticsItem from "../StatisticsItem";
+import StatisticsMenu from "../StatisticsMenu";
 
 const Statistics = () => {
   return (
-    <Section title="Estatísticas do mês">
-      <div className="flex flex-col gap-10 w-auto h-auto px-10 py-10 bg-white rounded-2xl">
-        <div className="flex gap-30">
-          <div className="inline-block w-[200px] h-[200px] bg-gray-300 rounded-full overflow-clip">
-            <StatisticsChart />
-          </div>
+    <>
+      <StatisticsMenu />
 
-          <div className="flex flex-col gap-3">
-            <StatisticsItem task="Tarefa 1" percentage={33} />
-            <StatisticsItem task="Tarefa 2" percentage={33} />
-            <StatisticsItem task="Tarefa 3" percentage={33} />
-            <StatisticsItem task="Tarefa 4" percentage={33} />
-            <StatisticsItem task="Tarefa 5" percentage={33} />
-            <StatisticsItem task="Tarefa 6" percentage={33} />
-            <StatisticsItem task="Tarefa 7" percentage={33} />
-            <StatisticsItem task="Tarefa 8" percentage={33} />
-            <StatisticsItem task="Tarefa 9" percentage={33} />
-            <StatisticsItem task="Tarefa 10" percentage={33} />
-            <StatisticsItem task="Tarefa 11" percentage={33} />
+      <Section title="Estatísticas do período">
+        <div className="flex flex-col gap-10 w-auto h-auto px-10 py-10 bg-white rounded-2xl">
+          <div className="flex gap-30">
+            <div className="inline-block w-[200px] h-[200px] bg-gray-300 rounded-full overflow-clip">
+              <StatisticsChart />
+            </div>
+
+            <div className="flex flex-col gap-3">
+              <StatisticsItem task="Tarefa 1" percentage={33} />
+              <StatisticsItem task="Tarefa 2" percentage={33} />
+              <StatisticsItem task="Tarefa 3" percentage={33} />
+              <StatisticsItem task="Tarefa 4" percentage={33} />
+              <StatisticsItem task="Tarefa 5" percentage={33} />
+              <StatisticsItem task="Tarefa 6" percentage={33} />
+              <StatisticsItem task="Tarefa 7" percentage={33} />
+              <StatisticsItem task="Tarefa 8" percentage={33} />
+              <StatisticsItem task="Tarefa 9" percentage={33} />
+              <StatisticsItem task="Tarefa 10" percentage={33} />
+              <StatisticsItem task="Tarefa 11" percentage={33} />
+            </div>
           </div>
         </div>
-      </div>
-    </Section>
+      </Section>
+    </>
   );
 };
 
---
+++ src/components/StatisticsMenu/index.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+const StatisticsMenu = () => {
+  const periods = [
+    { id: 0, name: "hoje" },
+    { id: 1, name: "semana" },
+    { id: 2, name: "mês" },
+    { id: 3, name: "ano" },
+  ];
+
+  const [selected, setSelected] = useState(0);
+  const [timePeriod, setTimePeriod] = useState(0);
+  const [year, setYear] = useState("2025");
+
+  const handleTimePeriod = (value) => {
+    if (timePeriod === value) return;
+    setTimePeriod(value);
+  };
+
+  const handleSelected = (value) => {
+    if (selected === value) return;
+    setSelected(!selected);
+  };
+
+  const handleYear = (value) => {
+    if (year === value) return;
+    setYear(value);
+  };
+
+  return (
+    <div className="flex gap-4 w-full h-auto p-5 mt-15 bg-[var(--sidebar-bg)] rounded-2xl">
+      {periods.map((period) => (
+        <button
+          className={`w-max px-7 py-2 rounded-4xl font-semibold uppercase cursor-pointer hover:bg-[var(--accent-opaque)] hover:text-white ${
+            timePeriod === period.id
+              ? "bg-[var(--accent-primary)] text-white"
+              : "bg-white text-[var(--fg-dark)]"
+          }`}
+          key={period.id}
+          onClick={() => handleTimePeriod(period.id)}
+        >
+          {period.name}
+        </button>
+      ))}
+
+      <div className="inline-block w-2 h-full border-r-2 border-[var(--border-dark)]"></div>
+
+      <select className="px-7 py-2 rounded-4xl border-0 bg-white text-[var(--fg-light)] font-semibold uppercase">
+        <option>2025</option>
+        <option>2024</option>
+        <option>2023</option>
+        <option>2022</option>
+        <option>2021</option>
+        <option>2020</option>
+      </select>
+    </div>
+  );
+};
+
+export default StatisticsMenu;
