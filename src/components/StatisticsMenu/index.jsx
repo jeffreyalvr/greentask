98908c31959047a0cb6870b22a834fcdb7f6e525
Jeffrey Ramo <matheusunitt@gmail.com>
2025-05-13 16:24:00 -0300

    Implementa troca de ano no History

--- src/components/StatisticsMenu/index.jsx
+++ src/components/StatisticsMenu/index.jsx
@@ -10,7 +10,9 @@ const StatisticsMenu = () => {
 
   const [selected, setSelected] = useState(0);
   const [timePeriod, setTimePeriod] = useState(0);
-  const [year, setYear] = useState("2025");
+  const [year, setYear] = useState("2023");
+
+  const currentYear = new Date().getFullYear().toString();
 
   const handleTimePeriod = (value) => {
     if (timePeriod === value) return;
@@ -22,14 +24,19 @@ const StatisticsMenu = () => {
     setSelected(!selected);
   };
 
-  const handleYear = (value) => {
+  const handleYear = (e) => {
+    const value = e.target.value;
     if (year === value) return;
     setYear(value);
   };
 
   return (
     <div className="flex gap-4 w-full h-auto p-5 mt-15 bg-[var(--sidebar-bg)] rounded-2xl">
-      <select className="px-7 py-2 rounded-4xl border-0 bg-white text-[var(--fg-light)] font-semibold uppercase">
+      <select
+        className="px-7 py-2 rounded-4xl border-0 bg-white text-[var(--fg-light)] font-semibold uppercase"
+        onChange={handleYear}
+        value={year}
+      >
         <option>2025</option>
         <option>2024</option>
         <option>2023</option>
@@ -40,19 +47,25 @@ const StatisticsMenu = () => {
 
       <div className="inline-block w-2 h-full border-r-2 border-[var(--border-dark)]"></div>
 
-      {periods.map((period) => (
-        <button
-          className={`w-max px-7 py-2 rounded-4xl font-semibold uppercase cursor-pointer hover:bg-[var(--accent-opaque)] hover:text-white ${
-            timePeriod === period.id
-              ? "bg-[var(--accent-primary)] text-white"
-              : "bg-white text-[var(--fg-dark)]"
-          }`}
-          key={period.id}
-          onClick={() => handleTimePeriod(period.id)}
-        >
-          {period.name}
-        </button>
-      ))}
+      {year !== currentYear ? (
+        <span className="w-full h-full flex items-center text-md text-[var(--fg-dark)] font-semibold">
+          Exibindo dados do ano {year}
+        </span>
+      ) : (
+        periods.map((period) => (
+          <button
+            className={`w-max px-7 py-2 rounded-4xl font-semibold uppercase cursor-pointer hover:bg-[var(--accent-opaque)] hover:text-white ${
+              timePeriod === period.id
+                ? "bg-[var(--accent-primary)] text-white"
+                : "bg-white text-[var(--fg-dark)]"
+            }`}
+            key={period.id}
+            onClick={() => handleTimePeriod(period.id)}
+          >
+            {period.name}
+          </button>
+        ))
+      )}
     </div>
   );
 };
