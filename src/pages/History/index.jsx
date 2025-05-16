9fb4775d8a4e3bbc9335b6b2572addc606414f3c
Jeffrey Ramo <matheusunitt@gmail.com>
2025-05-16 10:46:16 -0300

    Reorganiza History

--- src/pages/History/index.jsx
+++ src/pages/History/index.jsx
@@ -1,15 +1,12 @@
 import Sidebar from "../../components/Sidebar";
 import Statistics from "../../components/Statistics";
-import Activity from "../../components/Activity";
 
 const History = () => {
   return (
     <div className="flex h-screen">
       <Sidebar />
-      <main className="flex flex-col h-fit px-10">
+      <main className="w-full flex flex-col h-fit px-10">
         <Statistics />
-        <div className="inline-block h-2 border-dashed border-b-2 border-[var(--border-light)]"></div>
-        <Activity />
       </main>
     </div>
   );
