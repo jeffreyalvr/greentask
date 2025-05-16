546091f25c0f073ed7343e734ab52b85db948112
Jeffrey Ramo <matheusunitt@gmail.com>
2025-05-16 11:03:46 -0300

    Reorganiza Activity

--- src/components/Activity/index.jsx
+++ src/components/Activity/index.jsx
@@ -1,19 +1,16 @@
-import Section from "../Section";
 import ActivityItem from "../ActivityItem";
 
 const Activity = () => {
   return (
-    <Section title="Atividade de hoje">
-      <div className="flex gap-3 flex-wrap w-full">
-        <ActivityItem task="Tarefa 1" startedAt="12:00" totalTime={2} />
-        <ActivityItem task="Tarefa 2" startedAt="12:00" totalTime={2} />
-        <ActivityItem task="Tarefa 3" startedAt="12:00" totalTime={2} />
-        <ActivityItem task="Tarefa 4" startedAt="12:00" totalTime={2} />
-        <ActivityItem task="Tarefa 5" startedAt="12:00" totalTime={2} />
-        <ActivityItem task="Tarefa 6" startedAt="12:00" totalTime={2} />
-        <ActivityItem task="Tarefa 7" startedAt="12:00" totalTime={2} />
-      </div>
-    </Section>
+    <div className="flex gap-3 flex-wrap w-full">
+      <ActivityItem task="Tarefa 1" startedAt="12:00" totalTime={2} />
+      <ActivityItem task="Tarefa 2" startedAt="12:00" totalTime={2} />
+      <ActivityItem task="Tarefa 3" startedAt="12:00" totalTime={2} />
+      <ActivityItem task="Tarefa 4" startedAt="12:00" totalTime={2} />
+      <ActivityItem task="Tarefa 5" startedAt="12:00" totalTime={2} />
+      <ActivityItem task="Tarefa 6" startedAt="12:00" totalTime={2} />
+      <ActivityItem task="Tarefa 7" startedAt="12:00" totalTime={2} />
+    </div>
   );
 };
 
