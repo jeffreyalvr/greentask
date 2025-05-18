ce195b1367204e13c102cee606c6a1419eeb7a57
Jeffrey Ramo <matheusunitt@gmail.com>
2025-05-18 11:17:21 -0300

    Adiciona troca de state do store

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,12 @@
 import light_icon from "@assets/icons/light.svg";
 import dark_icon from "@assets/icons/dark.svg";
 
+import useThemeStore from "@/store/useThemeStore";
+
 const Footer = () => {
+  const theme = useThemeStore((state) => state.theme);
+  const setTheme = useThemeStore((state) => state.setTheme);
+
   return (
     <footer className="w-full flex flex-col gap-8 py-4 border-dashed border-t-2 border-[var(--border-dark)]">
       <span>
@@ -18,7 +23,14 @@ const Footer = () => {
       </span>
 
       <div className="flex gap-2 w-full">
-        <button className="font-semibold uppercase border-2 border-[var(--border-dark)] rounded-4xl px-3 py-1 text-[var(--fg-light)] hover:border-[var(--accent-opaque)]">
+        <button
+          className={`font-semibold uppercase border-2 rounded-4xl px-3 py-1 text-[var(--fg-light)] hover:border-[var(--accent-opaque)] ${
+            theme === "light"
+              ? "border-[var(--accent-primary)]"
+              : "border-[var(--border-dark)]"
+          }`}
+          onClick={() => setTheme("light")}
+        >
           <i className="w-[24px] h-[24px] flex items-center justify-center border-0">
             <img
               className="w-fit h-fit"
@@ -28,7 +40,14 @@ const Footer = () => {
             />
           </i>
         </button>
-        <button className="font-semibold uppercase border-2 border-[var(--border-dark)] rounded-4xl px-3 py-1 text-[var(--fg-light)] hover:border-[var(--accent-opaque)]">
+        <button
+          className={`font-semibold uppercase border-2 rounded-4xl px-3 py-1 text-[var(--fg-light)] hover:border-[var(--accent-opaque)] ${
+            theme === "dark"
+              ? "border-[var(--accent-primary)]"
+              : "border-[var(--border-dark)]"
+          }`}
+          onClick={() => setTheme("dark")}
+        >
           <i className="w-[24px] h-[24px] flex items-center justify-center border-0">
             <img
               className="w-fit h-fit"
